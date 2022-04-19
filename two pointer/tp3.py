b1cc569eb118d07060673eb19b789e1b1e179903
Isha Ranka <isharanka10@gmail.com>
2022-04-20 00:09:56 +0530

    19 april 2022

---
+++ two pointer/tp3.py
@@ -0,0 +1,35 @@
+'''You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+Return the maximum amount of water a container can store.
+
+Notice that you may not slant the container.
+
+ 
+
+Example 1:
+
+
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
+Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.'''
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        i=0
+        j=len(height)-1
+        maxarea=min(height[i],height[j])*(len(height)-1)
+        k=1
+        while(i<j):
+            maxx=min(height[i],height[j])*(len(height)-k)
+            if maxx >maxarea:
+                maxarea=maxx
+            if height[i]<height[j]:
+                i+=1
+                k+=1
+            else:
+                j-=1
+                k+=1
+        return maxarea
+
